786072e1a3bc69a57c0884a27901ebd6fda45843
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2018-06-30 15:59:49 +0500

    Board arragement updated.

--- Board.cs
+++ Board.cs
@@ -9,25 +9,25 @@ namespace BattleshipConsole
     public class Board
     {
         string[,] grid;
-        readonly char[] columns = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        readonly char[] rows = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
+        readonly char[] rows = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        readonly char[] columns = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
 
         public Board()
         {
             grid = new string[10, 10];
 
-            for (int j = 0; j < rows.Length; ++j)
-            for (int i = 0; i < columns.Length; ++i)
-                    grid[i,j] = rows[j].ToString() + columns[i].ToString();
+            for (int column = 0; column < columns.Length; ++column)
+                for (int row = 0; row < rows.Length; ++row)
+                    grid[column, row] = columns[column].ToString() + rows[row].ToString();
         }
 
         public void DisplayBoard()
         {
-            for (int i = 0; i < rows.Length; ++i)
+            for (int row = 0; row < rows.Length; ++row)
             {
                 Console.WriteLine();
-                for (int j = 0; j < columns.Length; ++j)
-                    Console.Write(grid[i, j] + " ");
+                for (int column = 0; column < columns.Length; ++column)
+                    Console.Write(grid[column, row] + " ");
             }
         }
     }
