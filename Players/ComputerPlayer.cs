c40375863cd28089e47f581a5aff87d896e1b30d
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2018-07-01 15:01:13 +0500

    Beginning to place computer ships

--- Players/ComputerPlayer.cs
+++ Players/ComputerPlayer.cs
@@ -13,18 +13,67 @@ namespace BattleshipConsole
             Name = Constants.COMPUTER_NAME;
         }
 
-        private void PlaceShip(Ship ship, char[] rows, char[] columns, string[,] grid)
+
+        private int GetRandomNumberInExludedRange(int start, int end, HashSet<int> exclude)
         {
+            end++;
+            var range = Enumerable.Range(start, end).Where(i => !exclude.Contains(i));
+            Random rand = new System.Random();
+            int index = rand.Next(start, end - exclude.Count);
+            return range.ElementAt(index);
+        }
+
+
+        private int GetRandomNumberInRange(int start, int end)
+        {
+            end++; // as the end is exclusive
+            Random rand = new System.Random();
+            int number = rand.Next(start, end);
+            return number;
+        }
+
+        private int GetRandomOrientation()
+        {
+            return GetRandomNumberInRange(1, 2);
+        }
+
+
+        private Position GetRandomGridPosition(Ship ship)
+        {
+            Position randPosition = new Position();
+            Point randPoint = new Point();
+            Random rand = new Random();
+
+            randPoint.Column = rand.Next(Board.Grid.GetLength(0));
+            randPoint.Row = rand.Next(Board.Grid.GetLength(1));
+
+            string cellContents = Board.Grid[randPoint.Column, randPoint.Row];
+            randPosition.Column = cellContents[0];
+            randPosition.Row = cellContents[1];
+
+            bool isValid = ship.ValidateStartPosition(randPosition, Board.Rows, Board.Columns, Board.Grid);
+
+            return randPosition;
+        }
+
+
+        private void PlaceShip(Ship ship)
+        {
+            ship.Orientation = GetRandomOrientation() == 1 ?
+                Ship.Orientations.Horizontal :
+                Ship.Orientations.Vertical;
+
+
 
         }
 
         public void PlaceAllShips()
         {
-            PlaceShip(Board.Destroyer1, Board.Rows, Board.Columns, Board.Grid);
+            PlaceShip(Board.Destroyer1);
             Board.DisplayBoard();
-            PlaceShip(Board.Destroyer2, Board.Rows, Board.Columns, Board.Grid);
+            PlaceShip(Board.Destroyer2);
             Board.DisplayBoard();
-            PlaceShip(Board.Battleship, Board.Rows, Board.Columns, Board.Grid);
+            PlaceShip(Board.Battleship);
         }
     }
 }
